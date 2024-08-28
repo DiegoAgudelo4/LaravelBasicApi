448ff4851d0fbfb81c8ea185b4abbb0bb2ceb029
DiegoAgudelo4 <diegoagudelo180@gmail.com>
2024-08-28 14:59:16 -0500

    Crud básico terminado

--- app/Models/Example.php
+++ app/Models/Example.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Example extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,13 @@
 // routes/api.php
 use App\Http\Controllers\ExampleController;
 
-// Route::get('/example', [ExampleController::class, 'index']);
-Route::get('/example', function(){
-    return 'example list';
-});
 Route::get('/test', function () {
     return response()->json(['message' => 'Test route working']);
 });
 
+Route::get('/example', [ExampleController::class, 'index']);
 
-Route::post('/example', [ExampleController::class, 'store']);
 Route::get('/example/{id}', [ExampleController::class, 'show']);
+Route::post('/example', [ExampleController::class, 'store']);
 Route::put('/example/{id}', [ExampleController::class, 'update']);
 Route::delete('/example/{id}', [ExampleController::class, 'destroy']);
